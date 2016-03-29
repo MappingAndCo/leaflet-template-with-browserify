4471548e50e3d53c17ccaca2902a0d792a423f8f
sigon <sigon426@users.noreply.github.com>
2016-03-29 12:01:09 +0200

    added leaflet-hash module

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 (function () {
    'use strict';
-    // require jquery module
+    // require modules
     var $ = require('jquery');
-    
-    // require leaflet module
     var L = require('leaflet');
+    var Hash = require('leaflet-hash');
 
     // Indicate leaflet the specific location of the images folder that it needs to render the page
     L.Icon.Default.imagePath = 'lib/leaflet/images/';
 
     // Create the map
     var map = L.map('map').setView([41.3921, 2.1705], 13);
-    
+
     // Use OpenStreetMap tiles and attribution
     var osmTiles = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="https://github.com/sigon426">sigon</a>';
-     
+
     // Create the basemap and add it to the map
     L.tileLayer(osmTiles, {
         maxZoom: 18,
         attribution: attribution
     }).addTo(map);
+
+    var hash = new L.Hash(map);
 
     // Add a marker
     L.marker([41.3921, 2.1705]).addTo(map)
--- bundle.js
+++ bundle.js
@@ -1,27 +1,28 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function () {
    'use strict';
-    // require jquery module
+    // require modules
     var $ = require('jquery');
-    
-    // require leaflet module
     var L = require('leaflet');
+    var Hash = require('leaflet-hash');
 
     // Indicate leaflet the specific location of the images folder that it needs to render the page
     L.Icon.Default.imagePath = 'lib/leaflet/images/';
 
     // Create the map
     var map = L.map('map').setView([41.3921, 2.1705], 13);
-    
+
     // Use OpenStreetMap tiles and attribution
     var osmTiles = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="https://github.com/sigon426">sigon</a>';
-     
+
     // Create the basemap and add it to the map
     L.tileLayer(osmTiles, {
         maxZoom: 18,
         attribution: attribution
     }).addTo(map);
+
+    var hash = new L.Hash(map);
 
     // Add a marker
     L.marker([41.3921, 2.1705]).addTo(map)
@@ -49,7 +50,7 @@
 
 }());
 
-},{"jquery":2,"leaflet":3}],2:[function(require,module,exports){
+},{"jquery":2,"leaflet":4,"leaflet-hash":3}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v2.2.2
  * http://jquery.com/
@@ -9894,6 +9895,170 @@ return jQuery;
 }));
 
 },{}],3:[function(require,module,exports){
+(function(window) {
+	var HAS_HASHCHANGE = (function() {
+		var doc_mode = window.documentMode;
+		return ('onhashchange' in window) &&
+			(doc_mode === undefined || doc_mode > 7);
+	})();
+
+	L.Hash = function(map) {
+		this.onHashChange = L.Util.bind(this.onHashChange, this);
+
+		if (map) {
+			this.init(map);
+		}
+	};
+
+	L.Hash.parseHash = function(hash) {
+		if(hash.indexOf('#') === 0) {
+			hash = hash.substr(1);
+		}
+		var args = hash.split("/");
+		if (args.length == 3) {
+			var zoom = parseInt(args[0], 10),
+			lat = parseFloat(args[1]),
+			lon = parseFloat(args[2]);
+			if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
+				return false;
+			} else {
+				return {
+					center: new L.LatLng(lat, lon),
+					zoom: zoom
+				};
+			}
+		} else {
+			return false;
+		}
+	};
+
+	L.Hash.formatHash = function(map) {
+		var center = map.getCenter(),
+		    zoom = map.getZoom(),
+		    precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+
+		return "#" + [zoom,
+			center.lat.toFixed(precision),
+			center.lng.toFixed(precision)
+		].join("/");
+	},
+
+	L.Hash.prototype = {
+		map: null,
+		lastHash: null,
+
+		parseHash: L.Hash.parseHash,
+		formatHash: L.Hash.formatHash,
+
+		init: function(map) {
+			this.map = map;
+
+			// reset the hash
+			this.lastHash = null;
+			this.onHashChange();
+
+			if (!this.isListening) {
+				this.startListening();
+			}
+		},
+
+		removeFrom: function(map) {
+			if (this.changeTimeout) {
+				clearTimeout(this.changeTimeout);
+			}
+
+			if (this.isListening) {
+				this.stopListening();
+			}
+
+			this.map = null;
+		},
+
+		onMapMove: function() {
+			// bail if we're moving the map (updating from a hash),
+			// or if the map is not yet loaded
+
+			if (this.movingMap || !this.map._loaded) {
+				return false;
+			}
+
+			var hash = this.formatHash(this.map);
+			if (this.lastHash != hash) {
+				location.replace(hash);
+				this.lastHash = hash;
+			}
+		},
+
+		movingMap: false,
+		update: function() {
+			var hash = location.hash;
+			if (hash === this.lastHash) {
+				return;
+			}
+			var parsed = this.parseHash(hash);
+			if (parsed) {
+				this.movingMap = true;
+
+				this.map.setView(parsed.center, parsed.zoom);
+
+				this.movingMap = false;
+			} else {
+				this.onMapMove(this.map);
+			}
+		},
+
+		// defer hash change updates every 100ms
+		changeDefer: 100,
+		changeTimeout: null,
+		onHashChange: function() {
+			// throttle calls to update() so that they only happen every
+			// `changeDefer` ms
+			if (!this.changeTimeout) {
+				var that = this;
+				this.changeTimeout = setTimeout(function() {
+					that.update();
+					that.changeTimeout = null;
+				}, this.changeDefer);
+			}
+		},
+
+		isListening: false,
+		hashChangeInterval: null,
+		startListening: function() {
+			this.map.on("moveend", this.onMapMove, this);
+
+			if (HAS_HASHCHANGE) {
+				L.DomEvent.addListener(window, "hashchange", this.onHashChange);
+			} else {
+				clearInterval(this.hashChangeInterval);
+				this.hashChangeInterval = setInterval(this.onHashChange, 50);
+			}
+			this.isListening = true;
+		},
+
+		stopListening: function() {
+			this.map.off("moveend", this.onMapMove, this);
+
+			if (HAS_HASHCHANGE) {
+				L.DomEvent.removeListener(window, "hashchange", this.onHashChange);
+			} else {
+				clearInterval(this.hashChangeInterval);
+			}
+			this.isListening = false;
+		}
+	};
+	L.hash = function(map) {
+		return new L.Hash(map);
+	};
+	L.Map.prototype.addHash = function() {
+		this._hash = L.hash(this);
+	};
+	L.Map.prototype.removeHash = function() {
+		this._hash.removeFrom();
+	};
+})(window);
+
+},{}],4:[function(require,module,exports){
 /*
  Leaflet, a JavaScript library for mobile-friendly interactive maps. http://leafletjs.com
  (c) 2010-2013, Vladimir Agafonkin
